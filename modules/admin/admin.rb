20d4e0512a4b435b16ec042de7790afc606371d9
Jaci Brunning <jaci.brunning@gmail.com>
2018-08-05 22:45:36 +0800

    Add uptime to admin panel

--- modules/admin/admin.rb
+++ modules/admin/admin.rb
@@ -1,9 +1,11 @@
 require 'webcore/cdn/extension'
 require 'webcore/db/authextension'
+require 'date'
 
 class AdminModule < WebcoreApp()
     register AuthExtension
     register CDNExtension
+    ADMIN_STARTUP = DateTime.now
 
     set :views, "#{File.dirname(__FILE__)}/views"
 
@@ -20,5 +22,20 @@ class AdminModule < WebcoreApp()
     get "/logout/?" do
         redirect "/" unless auth?
         logout!
+    end
+
+    def uptime
+        sec = ((DateTime.now - ADMIN_STARTUP)*24*60*60).to_i
+
+        mm, ss = sec.divmod(60)
+        hh, mm = mm.divmod(60)
+        dd, hh = hh.divmod(24)
+        strbuilder = []
+        strbuilder << "#{dd.round(0)}d" if dd > 0
+        strbuilder << "#{hh.round(0)}h" if hh > 0
+        strbuilder << "#{mm.round(0)}m" if mm > 0
+        strbuilder << "#{ss.round(0)}s"
+
+        strbuilder.join(" ")
     end
 end
